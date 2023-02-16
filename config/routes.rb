41fc57fefbe3981f7e29e3a9dd1c173606e2e512
EC2 Default User <121223610+mikihogu@users.noreply.github.com>
2023-02-16 02:37:15 +0000

    [Add]ルーティング作成 #21

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,31 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
   devise_for :users
+
+  root to: "homes#top"
+  get "/about"=>"homes#about"
+  resources :products, only: [:index, :show]
+  resource :customers, only: [:show, :edit, :update] do
+    get :check, on: :collection
+    get :withdrowl, on: :collection
+    resource :addresses, only: [:index, :edit, :create, :update, :destroy]
+  end
+  resources :cart_items, only: [:index, :update, :destroy, :create] do
+    delete :destroy_all, on: :collection
+  end
+  resources :orders, only: [:new, :create, :index, :show] do
+    post :check, on: :member
+    get :complete, on: :collection
+  end
+
+  namespace :admin do
+    root to: "admin/homes#top"
+    resources :products, except: [:destroy]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, onlyonly: [:show, :update]
+    resources :order_products, only: [:update]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
