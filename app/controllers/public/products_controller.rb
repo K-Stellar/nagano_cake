548433f1de8dda6ac4e42b991731abadc7a88c85
EC2 Default User <121223610+mikihogu@users.noreply.github.com>
2023-02-16 12:35:34 +0000

    [Add]First-commit 商品詳細ページ ページ全体仮実装

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -3,6 +3,14 @@ class Public::ProductsController < ApplicationController
   end
 
   def show
+    # @product = Product.find(params[:id])
+    @cart_item = CartItem.new
   end
-  
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :information, :price, :image)
+  end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,17 @@
 class Product < ApplicationRecord
+  has_many :cart_items, dependent: :destroy
+  has_many :order_items
+  belongs_to :genre
+  has_one_attached :image
+  validates :name, presence: true
+  validates :information, presence: true
+  validates :price, presence: true
 
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [100, 100]).processed
+  end
 end
