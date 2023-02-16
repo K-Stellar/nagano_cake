6bd54a8b8060b59a42c7a797904c458ad7197a87
Roger3104 <119906602+Roger3104@users.noreply.github.com>
2023-02-16 02:08:07 +0000

    Add Controllers #10

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,16 @@
+class OrdersController < ApplicationController
+  def new
+  end
+
+  def check
+  end
+
+  def complete
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,41 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'orders/show'
+  end
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+  end
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/edit'
+  end
+  namespace :admin do
+    get 'products/index'
+    get 'products/new'
+    get 'products/show'
+    get 'products/edit'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
+  get 'addresses/index'
+  get 'addresses/edit'
+  get 'orders/new'
+  get 'orders/check'
+  get 'orders/complete'
+  get 'orders/index'
+  get 'orders/show'
+  get 'cart_items/index'
+  get 'customers/show'
+  get 'customers/edit'
+  get 'customers/check'
+  get 'products/index'
+  get 'products/show'
+  get 'homes/top'
+  get 'homes/about'
+  get 'top/about'
   devise_for :admins
   devise_for :customers
   devise_for :users
