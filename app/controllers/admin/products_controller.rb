60a0e8d9bad8454413f41e6e32c584defca142ec
EC2 Default User <ec2-user@ip-172-31-14-254.ap-northeast-1.compute.internal>
2023-02-17 07:22:45 +0000

    #38 admin/products/new finish view

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -4,11 +4,27 @@ class Admin::ProductsController < ApplicationController
 
   def new
     @product=Product.new
+    @products=Product.all
   end
 
   def show
   end
 
   def edit
+  end
+  
+  def create
+    @product=Product.new(product_params)
+    if @product.save
+      redirect_to new_admin_product_path #商品詳細ページのパスへ変更予定
+    else
+      render ''
+    end
+  end
+  
+  private
+  
+  def product_params
+    params.require(:product).permit(:name, :information, :image, :genre_id, :price, :is_active)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,12 @@
 class Product < ApplicationRecord
+  has_one_attached :image
+  
+  validates :name, presence: true, uniqueness: true
+  validates :information, presence: true
+  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  
+  def get_image
+    (image.attachd?) ? image : 'no_image.jpg'
+  end
 
 end
