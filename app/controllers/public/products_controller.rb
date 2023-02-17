c33815553ae7d649702b32b935c9583ff2962ac7
Roger3104 <119906602+Roger3104@users.noreply.github.com>
2023-02-17 11:10:19 +0000

    [add] templates, edit index page

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,8 +1,16 @@
 class Public::ProductsController < ApplicationController
   def index
+    @products=Product.all
   end
 
   def show
+  end
+  
+  
+  private
+  
+  def product_params
+    params.require(:product).permit(:name, :information, :image, :genre_id, :price, :is_active)
   end
   
 end
