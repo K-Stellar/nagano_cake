8bfaa6b1855febd30d927ad65e7fdaf2415a0222
EC2 Default User <121223610+mikihogu@users.noreply.github.com>
2023-02-18 12:39:50 +0000

    ログイン中のみカート追加可能に変更

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,15 +3,15 @@ class Product < ApplicationRecord
   belongs_to :genre
   has_many :cart_items, dependent: :destroy
   has_many :order_items
-  
+
   validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 0}
 
   def get_image
     (image.attached?) ? image : 'no_image.jpg'
   end
 
-  def add_tax_price
-    (self.price*1.08).round
+  def include_tax_price
+    (price*1.1).floor
   end
 
 end
