79baf5344aff36fb8202afd964b1a7cb047d82c2
Roger3104 <119906602+Roger3104@users.noreply.github.com>
2023-02-18 01:09:37 +0000

    finished registration new

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -5,12 +5,12 @@ class Public::ProductsController < ApplicationController
 
   def show
   end
-  
-  
+
+
   private
-  
+
   def product_params
     params.require(:product).permit(:name, :information, :image, :genre_id, :price, :is_active)
   end
-  
+
 end
