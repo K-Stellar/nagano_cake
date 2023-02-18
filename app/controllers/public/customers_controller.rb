a78d107d083bc9ce14bbe8762e654de605ad3f38
Roger3104 <119906602+Roger3104@users.noreply.github.com>
2023-02-18 15:42:51 +0000

    feature mypage for customer

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,5 +1,6 @@
 class Public::CustomersController < ApplicationController
   def show
+    @customer = Customer.find_by(params[:id])
   end
 
   def edit
@@ -7,4 +8,16 @@ class Public::CustomersController < ApplicationController
 
   def check
   end
+
+
+
+
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_num)
+  end
+
+
 end
