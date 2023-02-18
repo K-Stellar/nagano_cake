d17643fb31ef92748f753a4e198351d1dd640364
EC2 Default User <121223610+mikihogu@users.noreply.github.com>
2023-02-18 11:05:05 +0000

    First commit

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,17 +1,28 @@
 class Public::CartItemsController < ApplicationController
+  before_action :authenticate_customer!
+  
   def index
+    # @cart_item.customer_id == current_customer.id
     @cart_items = CartItem.all
   end
 
   def create
-    @cart_item = CartItem.create(cart_item_params)
-    redirect_to cart_items_path
+    @cart_item = CartItem.new(cart_item_params)
+    if @cart_item.save
+      redirect_to cart_items_path
+    end
+  end
+  
+  def destroy_all
+    @cart_items = CartItem.all
+    @cart_items.destroy_all
+    redirect_to request.referer
   end
 
   private
   
   def cart_item_params
-    params.require(:cart_item).permit(:product_id, :price, :acount)
+    params.require(:cart_item).permit(:product_id, :price, :amount)
   end
   
 end
