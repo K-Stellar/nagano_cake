9112902a16f65246eb94d13e6318b584c68ae754
Ryoma-23 <120545081+Ryoma-23@users.noreply.github.com>
2023-02-18 03:26:09 +0000

    [Add]Adminの商品詳細画面 #55

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,16 @@
 class Product < ApplicationRecord
   has_one_attached :image
   belongs_to :genre
-  
+
   validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 0}
-  
+
   def get_image
     (image.attached?) ? image : 'no_image.jpg'
+  end
+
+  #税込価格の計算
+  def add_tax_price
+    (self.price * 1.10).round
   end
 
 end
