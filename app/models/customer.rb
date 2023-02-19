89154bfc99de38d650459c7d99b60b494b3fb6d8
Ryoma-23 <120545081+Ryoma-23@users.noreply.github.com>
2023-02-19 05:58:07 +0000

    [Add]管理者側の会員一覧画面を作成 #７４

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,5 +3,9 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  def full_name
+    self.last_name + self.first_name
+  end
 
 end
