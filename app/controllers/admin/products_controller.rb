8fdfd6c30a779385ca780a8d2417c46e6c2d6b11
EC2 Default User <ec2-user@ip-172-31-14-254.ap-northeast-1.compute.internal>
2023-02-21 05:24:05 +0000

    #106 [FINISH]管理者商品新規登録画面レイアウト

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -17,7 +17,6 @@ class Admin::ProductsController < ApplicationController
 
   def create
     @product=Product.new(product_params)
-    @product
     if @product.save
       redirect_to admin_product_path(@product.id)
     else
