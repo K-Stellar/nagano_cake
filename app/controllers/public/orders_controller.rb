a1f3696f1aeb8e840913157447f5f30223045caa
Roger3104 <119906602+Roger3104@users.noreply.github.com>
2023-02-21 05:51:40 +0000

    finish

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -7,18 +7,18 @@ class Public::OrdersController < ApplicationController
 
   def check
    @order = Order.new(order_params)
-   if params[:order]["address_option"] == "0"
+   if params[:order][:address_option] == "0"
      @order.postal_code = current_customer.postal_code
      @order.address = current_customer.address
      @order.name = current_customer.first_name + current_customer.last_name
 
-   elsif params[:order]["address_option"] == "1"
+   elsif params[:order][:address_option] == "1"
      @address = Address.find(params[:order]["address_id"])
      @order.postal_code = @address.postal_code
      @order.address = @address.address
      @order.name = @address.name
 
-   else
+   elsif params[:order][:address_option] == "2"
      @order.postal_code = params[:order]["postal_code"]
      @order.address = params[:order]["address"]
      @order.name = params[:order]["name"]
