3d914d4efd76dc9810ca83f0aa8cf10c43553424
EC2 Default User <ec2-user@ip-172-31-14-254.ap-northeast-1.compute.internal>
2023-02-21 07:44:52 +0000

    #91 入金ステータスプルダウンメニュー作成開始

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,4 +2,16 @@ class Admin::OrdersController < ApplicationController
   def show
     @order=Order.find(params[:id])
   end
+  
+  def update
+    order=order.find(params[:id])
+    order.update(order_paramas)
+    redirect_to request.referer
+  end
+  
+  private
+  
+  def order_paramas
+    paramas.require(:order).permit(:status)
+  end
 end
