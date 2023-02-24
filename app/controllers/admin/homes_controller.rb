fbe9832e43d80dbaf6592310e0698afa62e00360
Ryoma-23 <120545081+Ryoma-23@users.noreply.github.com>
2023-02-24 01:54:56 +0000

    [Update]注文履歴一覧の表示順を新しい順に変更

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,7 +1,7 @@
 class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
   def top
-   @orders = Order.page(params[:page]).per(10)
+   @orders = Order.page(params[:page]).order(created_at: :desc).per(10)
 
   end
 end
