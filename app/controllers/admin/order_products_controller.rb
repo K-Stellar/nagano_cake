562faf72e86175cb20af8084c8a9db0510fe8491
EC2 Default User <ec2-user@ip-172-31-14-254.ap-northeast-1.compute.internal>
2023-02-24 01:57:17 +0000

    #129 [FINISH]ステータス更新機能

--- app/controllers/admin/order_products_controller.rb
+++ app/controllers/admin/order_products_controller.rb
@@ -2,11 +2,22 @@ class Admin::OrderProductsController < ApplicationController
   before_action :authenticate_admin!
   
   def update
-    # order=Order.find(params[:id])
     @order_product=OrderProduct.find(params[:id])
-    # byebug
-    @order_product.update(order_product_params)
-    redirect_to request.referer
+    @order=Order.find(@order_product.order_id)
+    @order_products=@order.order_products.all
+    is_update=true
+    if @order_product.update(order_product_params)
+      @order.update(status: 2) if @order_product.makig_status == "製作中"
+      # 制作ステータスが「制作中」のときに、注文ステータスを「制作中」に更新する。
+      @order_products.each do |order_product| #紐づいている注文商品の制作ステータスを一つ一つeach文で確認していく。
+        if order_product.makig_status != "製作完了" #制作ステータスが「制作完了」ではない場合
+          is_update = false #上記で定義したアップデートする記述をfalseにする(下記で使用)
+        end
+      end
+      @order.update(status: 3) if is_update
+      # is_updateがtrueの場合に、注文ステータスが「発送準備中に更新される。
+    end
+      redirect_to request.referer
   end
   
   def order_product_params
