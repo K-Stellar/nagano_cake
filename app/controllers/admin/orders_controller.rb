b7a2f29743764d1900eaf2022d4cedee4b7e3a98
EC2 Default User <ec2-user@ip-172-31-14-254.ap-northeast-1.compute.internal>
2023-02-24 01:01:13 +0000

    #129 入金確認→制作待ち実装

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,7 +7,10 @@ class Admin::OrdersController < ApplicationController
   
   def update
     @order=Order.find(params[:id])
-    @order.update(order_params)
+    @order_products=OrderProduct.where(order_id: @order.id)
+    if @order.update(order_params)
+      @order_products.update_all(makig_status: 1) if @order.status == "入金確認"
+    end
     redirect_to request.referer
   end
   
